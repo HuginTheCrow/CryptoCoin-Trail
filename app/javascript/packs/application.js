c5cef87677246604bb33d2aaa27bda3751e12651
HuginTheCrow <huginthecrow@proton.me>
2022-04-29 20:18:01 +0000

    changelog.md

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,8 @@
+import "bootstrap";
+// import { homeScroll } from "../components/home";
+// homeScroll();
+import { loadDynamicBannerText } from '../components/banner';
+loadDynamicBannerText();
+
+
+
